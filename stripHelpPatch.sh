0fbb3ff7a1865afb29e648f21cbdfeb90abd3899
Christopher Panza <christopherpanza@gmail.com>
2015-09-05 22:07:14 -0400

    Update parsing script to be more robust
    Script now looks specifically for instances of t().
    Leaves out text not between quotes, aiming for translated string.
    Attempts to leave escaped quotes in string.

--- stripHelpPatch.sh
+++ stripHelpPatch.sh
@@ -14,15 +14,33 @@ input=$1
 output=$2
 
 rm $output
+rm temp.txt
 
-grep "^- " $input | while read -r line ; do
-  # Remove everything not in parenthesis
-  strip=$( sed -E 's/(^|\))[^\(\)]+(\(|$)/\1\2/' <<< "$line" )
-  # remove replacement values
-  strip=$( sed 's/, array.*//' <<< "$strip")
-  #remove first two characters ('
-  strip=$( cut -c 3- <<< "$strip")
-  #remove last character '
-  strip=${strip%?}
-  echo $strip >> $output
-done
+while read -r "line"; do
+    # Only use lines that start with - as these indicate patch deletions.
+    if grep -q '^- ' <<< "$line" ; then
+        # Separate cases for t('') and t("") since both are used.
+        if grep -q ".*[ \.\(]t('" <<< "$line" ; then
+            # Aim to retreive the string to be translated, ignoring replacement array.
+            # There are occurances of \' in the patch, which should not terminate the string.
+            # The most inner parenthesis attempts this:
+            # (\\\'|[^'])+ Select any character except ' including when preceeded by a slash.
+            strip=$( sed -E "s/.*t\('((\\\'|[^'])+)'.*/\1/" <<< "$line" )
+
+            # Resulting string should include ! (or this shouldn't be in the patch)
+            if grep -q '\!' <<< "$strip" ; then
+                echo "$strip" >> temp.txt
+            fi
+        elif grep -q '.*[ \.\(]t("' <<< "$line" ; then
+            strip=$( sed -E 's/.*t\("((\"|[^"])+)".*/\1/' <<< "$line" )
+            if grep -q '\!' <<< "$strip" ; then
+                echo "$strip" >> temp.txt
+            fi
+        fi
+    fi
+done < $input
+
+# Remove dupes
+# http://unix.stackexchange.com/questions/30173/how-to-remove-duplicate-lines-inside-a-text-file
+# http://stackoverflow.com/questions/19039036/how-to-delete-duplicate-lines-in-a-file-awk-sed-uniq-not-working-on-my-file
+awk '!x[$0]++' temp.txt > $output
